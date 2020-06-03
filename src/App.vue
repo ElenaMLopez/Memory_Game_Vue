60bb23508ea14d734d5d1263fa693ee1522ffa51
elenamlopez <elena.mateos1@gmail.com>
2020-06-03 21:57:52 +0200

    Add initial Home page

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,64 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>
+      <h1> Matching Game</h1>
+      <nav  class="nav-tabs" id="nav" ref="nav">
+        <router-link to="/">Home</router-link>
+
+      </nav>
+    </header>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: "home"
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+.title {
+  margin: 2em auto 0.5em;
+}
+
+.nav-tabs {
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    text-decoration: none;
+
+    &.router-link-active {
+      text-decoration: none;
+      color: #0b5891;
+      border-bottom: 2px solid #0b5891;
+    }
+  }
+}
+
+html {
+  box-sizing: border-box;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: inherit;
+}
+
+html,
+body {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+
+
 </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h2 class="home">HOME</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home'
+}
+</script>
